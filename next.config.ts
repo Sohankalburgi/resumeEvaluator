888b519ec1ce0408c4b1a0180f0787ae29559005
Sohankalburgi <sohankalburgi2004@gmail.com>
2025-03-01 22:50:55 +0530

    ff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Ensures best practices are followed
+},
+typescript: {
+    ignoreBuildErrors: true, // Prevents broken builds due to TypeScript errors
+},
+
   experimental: {
     serverComponentsExternalPackages: ['pdf2json'],
   },
